6bd4d25e00af9b04fa3f728abc989bcd4408bf45
최수빈 <giyom2ng@gmail.com>
2024-10-22 01:56:59 +0900

    index.html변경해봄

--- src/pages/img2.php
+++ src/pages/img2.php
@@ -1,10 +1,21 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Photo 2</title>
+    <title>Photo 1</title>
 </head>
 <body>
-    <h1>Photo 2 Page</h1>
-    <p>이 페이지는 Photo 2에 대한 내용을 보여줍니다.</p>
+
+<header onclick="window.location.href='https://zeroto.dothome.co.kr';" style="cursor: pointer;">
+        <h1>Back To the ZERO</h1>
+    </header>
+
+    
+    <p>777</p>
+
+    <!-- 이미지 버튼 -->
+    <a href="python777redirect.html">
+        <img src="../buttons/python777.png" alt="python777" width="600" height="400" style="cursor: pointer;">
+    </a>
+
 </body>
 </html>
