56184295257153e66791e788ad61faa7edfbdb13
최수빈 <giyom2ng@gmail.com>
2024-10-21 18:04:39 +0900

    uipath demo url 연결

--- src/pages/img5.php
+++ src/pages/img5.php
@@ -6,5 +6,11 @@
 <body>
     <h1>Photo 1 Page</h1>
     <p>이 페이지는 Photo 1에 대한 내용을 보여줍니다.</p>
+
+    <!-- 이미지 버튼 -->
+    <a href="uipath1redirect.php">
+        <img src="../buttons/uipath1.png" alt="Photo 1" width="600" height="400" style="cursor: pointer;">
+    </a>
+
 </body>
 </html>
